6849b873d3aabd123004f5a738698e12e09bf70c
Aiden Bettig <166055677+Richu727@users.noreply.github.com>
2025-02-19 21:47:05 +0000

    Redid contact form

--- js/custom.js
+++ js/custom.js
@@ -8,11 +8,44 @@ function getYear() {
 getYear();
 
 
-/** google_map js **/
-function myMap() {
-    var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
-        zoom: 18,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('dataForm');
+    const successCard = document.getElementById('successCard');
+
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+        
+        fetch('https://script.google.com/macros/s/AKfycbyVYkfTrr4MisfFqI08-I7gXS0i1X572psGLqYqISBKU4cC_iwa0Uid_cmTlYpQsvQ/exec', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: serializeForm(this)
+        })
+        .then(response => {
+            if (response.ok) {
+                form.style.display = 'none';
+                successCard.style.display = 'flex';
+            }
+        })
+        .catch(error => {
+            document.getElementById('status').textContent = 
+                'Request failed: ' + error.message;
+        });
+    });
+
+    function serializeForm(form) {
+        const inputs = form.querySelectorAll('input');
+        let serialized = '';
+        for (let i = 0; i < inputs.length; i++) {
+            if (inputs[i].name !== undefined) {
+                serialized += `${encodeURIComponent(inputs[i].name)}=${encodeURIComponent(
+                    inputs[i].value
+                )}&`;
+            }
+        }
+        return serialized.slice(0, -1);
+    }
+});
